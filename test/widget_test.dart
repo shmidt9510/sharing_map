41d1d8e8456e9fc7afe3fdc6c7dea6c10187a25e
shmidt9510 <shmidt9510@gmail.com>
2023-05-28 00:37:27 +0300

    buildable

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shaaring_map/main.dart';
+import 'package:sharing_map/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const myApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
